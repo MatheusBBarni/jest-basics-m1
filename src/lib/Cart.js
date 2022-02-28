80829a5ab70ee5d00eafc03f64a7dcfde0a584de
Matheus Barni <brehm.matheus@hotmail.com>
2022-02-28 02:13:22 -0300

    feat: finished Cart class

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -7,12 +7,61 @@ const Money = Dinero;
 Money.defaultCurrency = 'BRL';
 Money.defaultPrecision = 2;
 
+const calculatePercentageDiscount = (amount, { condition, quantity }) => {
+  if (condition?.percentage && quantity > condition.minimum) {
+    return amount.percentage(condition.percentage);
+  }
+
+  return Money({ amount: 0 });
+};
+
+const calculateQuantityDiscount = (amount, { quantity, condition }) => {
+  const isEven = quantity % 2 === 0;
+
+  if (condition?.quantity && quantity > condition?.quantity) {
+    return amount.percentage(isEven ? 50 : 40);
+  }
+
+  return Money({ amount: 0 });
+};
+
+const calculateDiscount = (amount, quantity, condition) => {
+  const list = Array.isArray(condition) ? condition : [condition];
+
+  const [higherDiscount] = list
+    .map(cond => {
+      if (cond.percentage) {
+        return calculatePercentageDiscount(amount, {
+          condition: cond,
+          quantity,
+        }).getAmount();
+      }
+
+      if (cond.quantity) {
+        return calculateQuantityDiscount(amount, {
+          condition: cond,
+          quantity,
+        }).getAmount();
+      }
+    })
+    .sort((a, b) => b - a);
+
+  return Money({ amount: higherDiscount });
+};
+
 export default class Cart {
   items = [];
 
   getTotal() {
     return this.items.reduce((acc, item) => {
-      return acc.add(Money({ amount: item.quantity * item.product.price }));
+      const amount = Money({ amount: item.quantity * item.product.price });
+      let discount = Money({ amount: 0 });
+
+      if (item.condition) {
+        discount = calculateDiscount(amount, item.quantity, item.condition);
+      }
+
+      return acc.add(amount).subtract(discount);
     }, Money({ amount: 0 }));
   }
 
@@ -29,23 +78,25 @@ export default class Cart {
   }
 
   checkout() {
-    const { total, items } = this.summary;
+    const { total, items } = this.summary();
 
     this.items = [];
 
     return {
-      total,
+      total: total.getAmount(),
       items,
     };
   }
 
   summary() {
-    const total = this.getTotal().getAmount();
+    const total = this.getTotal();
+    const formatted = total.toFormat('$0,0.00');
     const items = this.items;
 
     return {
       total,
       items,
+      formatted,
     };
   }
 }
