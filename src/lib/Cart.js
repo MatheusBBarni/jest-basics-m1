9b822959cffbc80480db8247ebe529536ea938cd
Matheus Barni <brehm.matheus@hotmail.com>
2022-02-28 02:29:36 -0300

    feat: added ndb for debugging and did some refactoring

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,53 +1,12 @@
 import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
+import { calculateDiscount } from './discount.utils';
 
 const Money = Dinero;
 
 Money.defaultCurrency = 'BRL';
 Money.defaultPrecision = 2;
-
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-  if (condition?.percentage && quantity > condition.minimum) {
-    return amount.percentage(condition.percentage);
-  }
-
-  return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { quantity, condition }) => {
-  const isEven = quantity % 2 === 0;
-
-  if (condition?.quantity && quantity > condition?.quantity) {
-    return amount.percentage(isEven ? 50 : 40);
-  }
-
-  return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-  const list = Array.isArray(condition) ? condition : [condition];
-
-  const [higherDiscount] = list
-    .map(cond => {
-      if (cond.percentage) {
-        return calculatePercentageDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      }
-
-      if (cond.quantity) {
-        return calculateQuantityDiscount(amount, {
-          condition: cond,
-          quantity,
-        }).getAmount();
-      }
-    })
-    .sort((a, b) => b - a);
-
-  return Money({ amount: higherDiscount });
-};
 
 export default class Cart {
   items = [];
